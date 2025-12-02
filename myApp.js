56b0ebcb769852135190c807c0e5b497c4d43587
Renzo Osmillo <osmillo08@gmail.com>
2025-12-02 18:00:21 +0800

    stylesrc to scrptsrc

--- myApp.js
+++ myApp.js
@@ -25,7 +25,7 @@ app.use(helmet.noCache());
 app.use(helmet.contentSecurityPolicy({
   directives: {
     defaultSrc: ["'self'"],
-    styleSrc: ["'self'", 'trusted-cdn.com'],
+    scriptSrc: ["'self'", 'trusted-cdn.com'],
   }
 }));
 

8231a8f03d0684f47a96a84073b3d23707495929
Renzo Osmillo <osmillo08@gmail.com>
2025-12-02 14:49:52 +0800

    added helmet as required

--- myApp.js
+++ myApp.js
@@ -48,6 +48,7 @@ const app = express();
 
 
 
+
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
@@ -58,5 +59,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`Info Sec App Server is running on port ${port}`);
 });

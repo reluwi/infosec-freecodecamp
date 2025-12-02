9f4846274f262ec21aec3f6ec09283e4a3d8eaf9
Renzo Osmillo <osmillo08@gmail.com>
2025-12-02 17:13:45 +0800

    added hsts

--- myApp.js
+++ myApp.js
@@ -12,7 +12,11 @@ app.use(helmet.noSniff());
 
 app.use(helmet.ieNoOpen());
 
-
+const timeInSeconds = 90 * 24 * 60 * 60; // 90 days
+app.use(helmet.hsts({
+  maxAge: timeInSeconds,
+  force: true,
+}));
 
 
 

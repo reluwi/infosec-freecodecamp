bb866ac109836e505bddad99a431298381ba5669
Renzo Osmillo <osmillo08@gmail.com>
2025-12-02 18:16:50 +0800

    configured using helmet parent

--- myApp.js
+++ myApp.js
@@ -2,33 +2,43 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-app.use(helmet.hidePoweredBy());
+// app.use(helmet.hidePoweredBy());
 
-app.use(helmet.frameguard({ action: 'deny' }));
+// app.use(helmet.frameguard({ action: 'deny' }));
 
-app.use(helmet.xssFilter());
+// app.use(helmet.xssFilter());
 
-app.use(helmet.noSniff());
+// app.use(helmet.noSniff());
 
-app.use(helmet.ieNoOpen());
+// app.use(helmet.ieNoOpen());
 
-const timeInSeconds = 90 * 24 * 60 * 60; // 90 days
-app.use(helmet.hsts({
-  maxAge: timeInSeconds,
-  force: true,
+// const timeInSeconds = 90 * 24 * 60 * 60; // 90 days
+// app.use(helmet.hsts({
+//   maxAge: timeInSeconds,
+//   force: true,
+// }));
+
+// app.use(helmet.dnsPrefetchControl());
+
+// app.use(helmet.noCache());
+
+// app.use(helmet.contentSecurityPolicy({
+//   directives: {
+//     defaultSrc: ["'self'"],
+//     scriptSrc: ["'self'", 'trusted-cdn.com'],
+//   }
+// }));
+
+app.use(helmet({
+  frameguard: { action: 'deny' },
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'trusted-cdn.com'],
+    }
+  },
+  noCache: true 
 }));
-
-app.use(helmet.dnsPrefetchControl());
-
-app.use(helmet.noCache());
-
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", 'trusted-cdn.com'],
-  }
-}));
-
 
 
 
